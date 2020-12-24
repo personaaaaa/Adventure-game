d1c0979853e08c5586a8965bac0aea8da32c0fb5
Li Wenshu <1924494728@qq.com>
2020-12-24 10:06:51 +0800

    “头文件提取”

---
+++ disaster.h
@@ -0,0 +1,8 @@
+//
+// Created by Li Wenshu on 2020/12/24.
+//
+
+#ifndef HELLOWORLD_DISASTER_H
+#define HELLOWORLD_DISASTER_H
+
+#endif //HELLOWORLD_DISASTER_H
---
+++ gem.h
@@ -0,0 +1,8 @@
+//
+// Created by Li Wenshu on 2020/12/24.
+//
+
+#ifndef HELLOWORLD_GEM_H
+#define HELLOWORLD_GEM_H
+
+#endif //HELLOWORLD_GEM_H
